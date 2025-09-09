db3113b363fa2ab52f34b041de5f74b690c2b696
molfaryura <oleksandrr1996@protonmail.com>
2025-09-09 11:21:06 +0300

    Add logging

--- tests/test_upnp_basic.py
+++ tests/test_upnp_basic.py
@@ -5,10 +5,13 @@ import miniupnpc
 import utils
 
 
-def test_valid_ip_address(igd: miniupnpc.UPnP, local_ip_address: str):
+def test_valid_ip_address(log, igd: miniupnpc.UPnP, local_ip_address: str):
     """Test that a valid IP address is returned correctly."""
 
     external_ip_address = igd.externalipaddress()
+
+    log.info("IGD IPv4 address: " + external_ip_address)
+    log.info("Local IPv4 address: " + local_ip_address)
 
     assert (
         utils.is_ip_address_valid(external_ip_address)
