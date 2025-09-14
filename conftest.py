4fc0139f65dcb8ddfafe367453d4ceb633538f0e
molfaryura <oleksandrr1996@protonmail.com>
2025-09-14 10:02:26 +0300

    Add config_upnp(), update idg()

--- conftest.py
+++ conftest.py
@@ -8,6 +8,8 @@ import pytest
 
 import miniupnpc
 
+from upnp_setup import upnp_manager
+
 
 @pytest.fixture(scope="session")
 def log() -> logging.Logger:
@@ -18,13 +20,32 @@ def log() -> logging.Logger:
 
 
 @pytest.fixture(scope="session")
+def config_upnp(log: logging.Logger):
+    """Fixture to configure UPnP before and after tests."""
+
+    (
+        log.info("UPnP enabled")
+        if upnp_manager.turn_on_upnp() is not None
+        else log.info("UPnP is already enabled")
+    )
+
+    yield upnp_manager
+
+    (
+        log.info("UPnP disabled")
+        if upnp_manager.turn_off_upnp() is not None
+        else log.info("UPnP is already disabled")
+    )
+
+
+@pytest.fixture(scope="session")
 def igd() -> miniupnpc.UPnP:
     """Fixture to get IGD instance"""
-
     upnp = miniupnpc.UPnP()
-    upnp.discoverdelay = 800
+    upnp.discoverdelay = 2000
 
     try:
+        upnp.discover()
         upnp.selectigd()
     except Exception as e:
         pytest.skip(f"UPnP discovery and selection failed: {e}")
